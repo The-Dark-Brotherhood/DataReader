5b084e9646930af3783f5b503e0c08b1c3a0f226
CheckRates <gabsantana23@gmail.co>
2020-03-15 16:15:10 -0700

    :sparkles: Add shared memory (master list)

--- inc/dataReader.h
+++ inc/dataReader.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
@@ -5,8 +6,26 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
+#include <sys/shm.h>
 
 #define KEY_PATH     "/home/gabsantana/Downloads"
-#define MSG_ID_ERROR -1
+#define ID_ERROR      -1
+
+#define SHM_KEYID     16535
 
 #define TIMEOUT       15
+
+#define MAX_DC_ROLES  10
+
+typedef struct
+{
+  pid_t dcProcessID;
+  long lastTimeHeardFrom;   // TODO: Not sure if this the best data type for that
+} DCInfo;
+
+typedef struct
+{
+  int msgQueueID;
+  int numberOfDCs;
+  DCInfo dc[MAX_DC_ROLES];
+} MasterList;
--- src/dataReader.c
+++ src/dataReader.c
@@ -2,30 +2,63 @@
 
 int main(int argc, char const *argv[])
 {
-  // Grab key for identifying the message queue
-  key_t key = ftok(KEY_PATH, 'G');
+  // MESSAGE QUEUE;
+  // Grabs key and check for existing queue, creates if necessary
+  key_t msgKey = ftok(KEY_PATH, 'G');
   int msgID = 0;
 
-  if(key == MSG_ID_ERROR)
+  if(msgKey == ID_ERROR)
   {
     printf("ERROR: Unable to generate a key for message queue\n");
-    return MSG_ID_ERROR;
+    return ID_ERROR;
   }
 
   // Grab or Create message key
-  if(msgID = msgget(key, 0) == -1)
+  if(msgID = msgget(msgKey, 0) == -1)
   {
-    msgID = msgget(key, (IPC_CREAT | 660));
+    msgID = msgget(msgKey, (IPC_CREAT | 660));
     if(msgID == -1)
     {
       printf("ERROR: Unable to create queue\n%s\n", strerror(errno));
+      return errno;
     }
   }
 
-  // Create master list (shared memory)
+  printf("Message: %d\n",msgID );
+
+  // MASTER LIST;
+  // Grabs key and check for existing shared memory, creates if necessary
+  key_t shmKey = ftok(KEY_PATH, SHM_KEYID);
+  int shmID = 0;
+  if(shmKey == ID_ERROR)
+  {
+    printf("ERROR: Unable to generate a key for shared memory\n");
+    return ID_ERROR;
+  }
+
+  // Grab or Create shared memory
+  MasterList* shList = NULL;
+  if(shmID = shmget(shmKey, sizeof(MasterList), 0) == -1)
+  {
+    shmID = shmget(shmKey, sizeof(MasterList), (IPC_CREAT | 0660));
+    if(shmID == -1)
+    {
+      printf("ERROR: Unable to create shared memory\n%s\n", strerror(errno));
+      return errno;
+    }
+  }
+  shList = (MasterList *)shmat (shmID, NULL, 0);
 
   // Sleep for timeout seconds
+  printf("Waiting for %d...\n", TIMEOUT);
   sleep(TIMEOUT);
+
+  // Listening loop
+
+  // Clean up and exit
+  shmdt(shList);
+  shmctl (shmID, IPC_RMID, 0);
+  msgctl (msgID, IPC_RMID, (struct msqid_ds*)NULL);
 
   return 0;
 }
