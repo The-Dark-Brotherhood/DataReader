822bff9e037b76aa2371f83072ca2658ef750e64
CheckRates <gabsantana23@gmail.co>
2020-03-15 12:27:01 -0700

    :sparkles: Add Message queue creation

--- inc/dataReader.h
+++ inc/dataReader.h
@@ -1,1 +1,8 @@
 #include <stdio.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+#define KEY_PATH     "/home/gabsantana/Downloads"
+#define MSG_ID_ERROR -1
--- src/dataReader.c
+++ src/dataReader.c
@@ -1,6 +1,30 @@
 #include "../inc/dataReader.h"
 
-int main(int argc, char const *argv[]) {
-  printf("Hello World");
+int main(int argc, char const *argv[])
+{
+  // Grab key for identifying the message queue
+  key_t key = ftok(KEY_PATH, 'G');
+  int msgID = 0;
+
+  if(key == MSG_ID_ERROR)
+  {
+    printf("ERROR: Unable to generate a key for message queue\n");
+    return MSG_ID_ERROR;
+  }
+
+  // Grab or Create message key
+  if(msgID = msgget(key, 0) == -1)
+  {
+    msgID = msgget(key, (IPC_CREAT | 660));
+    if(msgID == -1)
+    {
+      printf("ERROR: Unable to create queue\n%s\n", strerror(errno));
+    }
+  }
+
+  // Create master list (shared memory)
+
+  // Wait 15 seconds 
+
   return 0;
 }
