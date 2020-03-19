1de48dcdec0de2ee5803672a72effb1088b56f5b
CheckRates <gabsantana23@gmail.co>
2020-03-19 11:13:48 -0700

    :fire: Remove messageHandling.c

--- src/messageHandling.c
+++
@@ -1,9 +0,0 @@
-#include "../inc/dataReader.h"
-
-/*
-void processMessage(MasterList* list, msgData msg)
-{
-  list->dc[lastDCindex].dcProcessID = msg.clientId;
-  printf("Received Process ID: %d\n", (int)list->dc[lastDCindex].dcProcessID);
-}
-*/
